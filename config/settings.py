866700bc2bce39ce1e8bd900516835cec74e2b25
EkaterinaZaitcev <katyat1@mail.ru>
2025-03-13 16:50:50 +1000

    Созданы user , подключены urls

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
+AUTH_USER_MODEL = 'users.CustomsUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from user.apps import UserConfig
 from user.views import RegisterView
 
-app_name =UserConfig.name
+app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
